9a50427197231c9006ef8d6aa593150c9049740a
”1146-yamagishi” <rikurikurikuriku0515@gmail.com>
2020-06-04 13:58:24 +0000

    first commit

--- resources/views/admin/gin/create.blade.php
+++ resources/views/admin/gin/create.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Mykadai</title>
+        <title>Mykadai3</title>
     </head>
     <body>
-        <h1>Myニュースkadai</h1>
+        <h1>Myニュースgisi</h1>
     </body>
 </html>
